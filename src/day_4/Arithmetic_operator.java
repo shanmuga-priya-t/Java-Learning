4e33262006dc080dcae87df3dbba3b31e63caea2
shanmuga-priya-t <shanmugapriya3442@gmail.com>
2026-09-22 22:42:24 +0530

    Day 4 Java basics

---
+++ src/day_4/Arithmetic_operator.java
@@ -0,0 +1,16 @@
+package day_4;
+
+public class Arithmetic_operator {
+
+	public static void main(String[] args) {
+	
+		        
+		        double a=5.2;
+		        double b=2.6;
+		        double c=a/b;
+
+		        
+		       
+		        System.out.println("a = " + a + ", b = " + b + ", c = " + c);
+		    }
+		}
---
+++ src/day_4/Type_casting_part2.java
@@ -0,0 +1,26 @@
+package day_4;
+
+public class Type_casting_part2 {
+
+	public static void main(String[] args) {
+	
+			//int to string
+			int number1=789;
+			//double to String
+			double number2=789;
+			//boolean to string
+			boolean isValid=true;
+			String text1=String.valueOf(number1);
+			String text2=String.valueOf(number2);
+			String text3=String.valueOf(isValid);
+			
+			System.out.println(text1);
+			System.out.println(text2);
+			System.out.println(text3);
+	
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
