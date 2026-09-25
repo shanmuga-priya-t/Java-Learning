d0bdd683808a9d6c4e4118fa704cde985c21414a
shanmuga-priya-t <shanmugapriya3442@gmail.com>
2026-09-25 23:10:34 +0530

    Day 7 Java basics

---
+++ src/day_7/Logical_operator.java
@@ -0,0 +1,14 @@
+package day_7;
+
+public class Logical_operator {
+
+	public static void main(String[] args) {
+		
+		        int b1 = 10;
+		        int b2 = 10;
+		        boolean b3 = !((b1 + b2) > (b1 * b2));
+		        
+		        // Don\'t change the line below
+		        System.out.println("b3 = " + b3);
+		    }
+		}
